28c21f0cf74bb43ef138f74ca6a37f205c046273
GidyMane <muragegideon2000@gmail.com>
2023-10-07 01:42:58 +0300

    Insert get element by id

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here!
+// const element= document.createElement('div');
+// document.body.append(element);
+
+// const ul = document.createElement('ul');
+
+// for (let i=0; i <3; i ++ ){
+//     const li = document.createElement('li');
+//     li.textContent =(i +1).toString();
+//     ul.append(li);
+// }
+// element.append(ul);
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+// element.className = "pet-listing dog";
+
+const element = document.getElementById('main');
+
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent= 'Gideon is the champion';
+document.body.append(newHeader);
+
+
+
+
+
+
